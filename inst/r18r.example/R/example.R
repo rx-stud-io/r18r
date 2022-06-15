9356e30d58683a85a4ac60eff769c6c366d338a8
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2022-06-15 23:25:54 +0200

    fix typo from rebranding

--- inst/r18r.example/R/example.R
+++ inst/r18r.example/R/example.R
@@ -1,5 +1,5 @@
 #' Return a translation
-#' @importFrom r18n translations_import
+#' @importFrom r18r translations_import
 #' @export
 r18r_example_translation <- function(language = translate_get_language('r18r.example')) {
     translate(T('Text from r18r.example', 'To demo the r18r package.'), language)
