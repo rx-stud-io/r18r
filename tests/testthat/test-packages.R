6fa3f8f1402bac4ea3fdf399a985cb170f6d1677
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2022-06-15 22:37:56 +0200

    try loading pkg outside of expect

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -6,7 +6,7 @@ po_folder <- system.file('i18n', package = 'r18r')
 po_file <- file.path(po_folder, 'en.po')
 
 test_that('read PO files', {
-    expect_error(load_all(system.file('r18r.example', package = 'r18r')), NA)
+    load_all(system.file('r18r.example', package = 'r18r'))
     expect_equal(r18r_example_translation(), 'Egy szöveg egyenes az r18r.example csomagból')
     expect_equal(r18r_example_translation('en'), 'Text from r18r.example')
 })
