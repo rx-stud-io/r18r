026eb35ba951027dcd3b88fb74ffcef448810880
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2022-06-16 00:17:07 +0200

    only test r18r.example pkg if installed

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -2,11 +2,9 @@ library(r18r)
 library(testthat)
 library(devtools)
 
-po_folder <- system.file('i18n', package = 'r18r')
-po_file <- file.path(po_folder, 'en.po')
-
-test_that('read PO files', {
-    load_all(system.file('r18r.example', package = 'r18r'))
-    expect_equal(r18r_example_translation(), 'Egy szöveg egyenes az r18r.example csomagból')
-    expect_equal(r18r_example_translation('en'), 'Text from r18r.example')
-})
+if (require(r18r.example)) {
+    test_that('read PO files', {
+        expect_equal(r18r_example_translation(), 'Egy szöveg egyenes az r18r.example csomagból')
+        expect_equal(r18r_example_translation('en'), 'Text from r18r.example')
+    })
+}
